939f35818248476f8ecec355d2fd19bb28d28124
Kyle Williams <willik23@polyprep.org>
2023-04-18 17:51:00 -0400

    Delete IMU.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -93,11 +93,14 @@ public class SampleMecanumDrive extends MecanumDrive {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
         }
 
+        // The IMU does not serve a purpose in three wheel odometry. Thus, it would be ideal to get
+        // rid of the default initialization in SampleMecanumDrive. IMU initialization can add 2-3
+        // seconds to the opmode initialization and it's quite annoying.
         // TODO: adjust the names of the following hardware devices to match your configuration
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
-        imu.initialize(parameters);
+        // imu = hardwareMap.get(IMU.class, "imu");
+        // IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+        //         DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
+        // imu.initialize(parameters);
 
         leftFront = hardwareMap.get(DcMotorEx.class, "FLW");
         leftRear = hardwareMap.get(DcMotorEx.class, "BLW");
@@ -289,9 +292,11 @@ public class SampleMecanumDrive extends MecanumDrive {
         rightFront.setPower(v3);
     }
 
+//    The IMU does not serve a purpose in three wheel odometry.
     @Override
     public double getRawExternalHeading() {
-        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        return 0;
+//        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
     }
 
     @Override
